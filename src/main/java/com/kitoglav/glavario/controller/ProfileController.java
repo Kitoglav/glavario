48b61216cc053675a9a635b414c8588dadebb0a9
Kitoglav <vlad_lanayev@vk.com>
2025-04-24 04:11:10 +0300

    profile page TEST

--- src/main/java/com/kitoglav/glavario/controller/ProfileController.java
+++ src/main/java/com/kitoglav/glavario/controller/ProfileController.java
@@ -7,27 +7,33 @@ import com.kitoglav.glavario.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class ProfileController {
     private final JwtComponent jwtComponent;
     private final UserService userService;
 
     @GetMapping("/profile")
-    public ResponseEntity<?> profile(@CookieValue(name = "jwt", required = false) String token) {
+    public String profile(@CookieValue(name = "jwt", required = false) String token, Model model) {
         if (token == null || !jwtComponent.validateToken(token)) {
             throw new ApiResponseException(HttpStatus.UNAUTHORIZED, "Недействительный токен");
         }
         String username = jwtComponent.getUsernameFromToken(token);
-        return userService.getUser(username)
-                .map(User::convert)
-                .map(user -> ResponseEntity.ok(Map.of("username", user.getUsername(), "roles", user.getRoles())))
+        User user = userService.getUser(username)
                 .orElseThrow(() -> new ApiResponseException(HttpStatus.INTERNAL_SERVER_ERROR, "Пользователь с именем {%S} не найден".formatted(username)));
+        model.addAttribute("auth", user != null);
+        if(user != null) {
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("roles", user.getAuthorities());
+        }
+        return "profile";
     }
 }
